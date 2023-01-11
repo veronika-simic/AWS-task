e421ea080297af99c306f6eedc2c7b3f997bc3d4
veronika-simic <veronikasimic21@gmail.com>
2023-01-11 10:16:41 +0100

    store rotated image to bucket

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-const SQSWorker = require("sqs-worker");
+/* const SQSWorker = require("sqs-worker");
 
 const options = {
   url: "https://sqs.us-east-1.amazonaws.com/222621649155/ImageQueue.fifo",
@@ -19,3 +19,42 @@ function worker(notifi, done) {
 
   done(null, success);
 }
+ */
+
+/* get image from s3 */
+const sharp = require("sharp");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3({ region: "us-east-1" });
+const fs = require("fs");
+s3.getObject(
+  { Bucket: "images-bucket-vera", Key: "AWS.png" },
+  (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  }
+);
+
+const uploadFile = (fileName, bucketName) => {
+    let fileContent = fs.readFileSync(fileName);
+    fileContent = sharp(fileContent).rotate(180);
+    const params = {
+      Bucket: bucketName,
+      Key: "AWS.png",
+      Body: fileContent,
+    };
+  
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
+  };
+  
+  uploadFile('AWS.png', 'rotated-images')
+  
+/* update state and url path in dynamoDB */
