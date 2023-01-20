3627ce226d124a4a1c963ad09b51d3fc0678b832
veronika-simic <veronikasimic21@gmail.com>
2023-01-20 11:54:03 +0100

    update authentication

--- auth.js
+++ auth.js
@@ -1,19 +1,16 @@
-module.exports = (req, res)  =>  {
-  const image = req.files.file.name
-  // Array of allowed files
-  const array_of_allowed_files = ["png", "jpeg", "jpg", "gif"];
-  // Allowed file size in mb
-  const allowed_file_size = 2;
-  // Get the extension of the uploaded file
-  const file_extension = image.slice(
-    ((image.lastIndexOf(".") - 1) >>> 0) + 2
-  );
+module.exports = (req, res) => {
+  const image = req.files.file.name;
 
-  // Check if the uploaded file is allowed
-  if (
-    !array_of_allowed_files.includes(file_extension)
-  ) {
-    throw Error("Invalid file type. Allowed file types are png jpeg jpg and gif");
+  const array_of_allowed_files = ["png", "jpeg", "jpg", "gif"];
+
+  const allowed_file_size = 2;
+
+  const file_extension = image.slice(((image.lastIndexOf(".") - 1) >>> 0) + 2);
+
+  if (!array_of_allowed_files.includes(file_extension)) {
+    throw Error(
+      "Invalid file type. Allowed file types are png, jpeg, jpg, and gif"
+    );
   }
 
   if (image.size / (1024 * 1024) > allowed_file_size) {
